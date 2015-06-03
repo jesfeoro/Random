5ad2c227711ef04c26e6456dd5262b66f8618520
jesus <jesfeoro@gmail.com>
2015-06-03 13:15:38 +0200

    segundo commit

--- random.js
+++ random.js
@@ -1,7 +1,8 @@
 // Math.random() devuelve número aleatorio entre 0 y 1
 var numero  = Math.random();
+var str='MAYOR que 0.5'
 if (numero <= 0.5) {
-  console.log('\n' + numero + 'MENOR que 0.5');
-}else {
-  console.log('\n' + numero + 'MAYOR que 0.5');
+  str = 'MENOR que 0,5';
 }
+
+console.log('\n' + numero + str + '\n');
